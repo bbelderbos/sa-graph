ed1fb2cbabcccf5d9f48cdb4721bb50843f830b0
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-03 21:57:02 +0200

    have it work with 3 graph types

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from collections import Counter
 from datetime import datetime
-import math
+from itertools import cycle, islice
+from math import ceil
 import os
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, MetaData, and_
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 load_dotenv()
-app = Flask(__name__)
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 TABLE = os.environ["TABLE"]
 COLUMN = os.environ["COLUMN"]
+TEMPLATE_FOLDER = "templates"
+DEFAULT_TEMPLATE = "line"
 
 engine = create_engine(DATABASE_URL)
 session = sessionmaker(engine)()
@@ -26,6 +28,8 @@ Base = automap_base(metadata=metadata)
 Base.prepare()
 
 SA_TABLE = getattr(Base.classes, TABLE)
+
+app = Flask(__name__, template_folder=TEMPLATE_FOLDER)
 
 
 def get_data():
@@ -44,15 +48,45 @@ def get_data():
     return sorted(cnt.items())
 
 
+def is_valid_template(template_name):
+    template_names = [os.path.splitext(template)[0]
+                      for template in os.listdir(TEMPLATE_FOLDER)]
+    return template_name in template_names
+
+
 def calculate_max_height_graph(values):
     max_value = max(values)
-    return int(math.ceil(max_value / 100.0)) * 100
+    return int(ceil(max_value / 100.0)) * 100
+
+
+def get_colors(values):
+    colors = ["#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+              "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+              "#C71585", "#FF4500"]
+    return islice(cycle(colors), len(values))
 
 
 @app.route('/')
-def show_data():
+@app.route('/<template_name>')
+def show_data(template_name=None):
+    template_name = template_name if template_name else DEFAULT_TEMPLATE
+    if not is_valid_template(template_name):
+        abort(400, 'Not a valid template')
+
     data = get_data()
     labels, values = zip(*data)
-    return render_template('index.html', title=f"{TABLE} - {COLUMN}",
-                           labels=labels, values=values,
-                           max_height=calculate_max_height_graph(values))
+    max_height = calculate_max_height_graph(values)
+
+    kwargs = {}
+    if template_name == "pie":
+        colors = get_colors(values)
+        kwargs = {
+            "set": zip(values, labels, colors)
+        }
+
+    return render_template(f'{template_name}.html',
+                           title=f"{TABLE} - {COLUMN}",
+                           labels=labels,
+                           values=values,
+                           max_height=max_height,
+                           **kwargs)
